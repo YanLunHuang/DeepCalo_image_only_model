4764171385375dcc355aaa95f2bc389422804c55
YanLunHuang <YanLunHuang>
2023-01-15 18:20:29 +0800

    send five set of input with single stream

---
+++ src/weights/b42.h
@@ -0,0 +1,15 @@
+//Numpy array shape [1]
+//Min 0.972656250000
+//Max 0.972656250000
+//Number of zeros 0
+
+#ifndef B42_H_
+#define B42_H_
+
+#ifndef __SYNTHESIS__
+bias42_t b42[1];
+#else
+bias42_t b42[1] = {0.97265625};
+#endif
+
+#endif
